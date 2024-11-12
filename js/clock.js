e292f5fff7f7069b43eecbbd31183f9f8da688a1
yeonjae <pyj3740@gmail.com>
2024-11-12 11:37:58 +0900

    4.2: PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); // 웹사이트가 로드되자마자 getClock을 실행하고 매 초마다 실행
