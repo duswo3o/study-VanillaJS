ff1c3658a6219008776eef78f17021d5a130ce6e
yeonjae <pyj3740@gmail.com>
2024-11-14 12:30:55 +0900

    1.4: Drawing Project One

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -4,10 +4,20 @@ const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용
 canvas.width = 800;
 canvas.height = 800;
 
-// 사각형 만들기
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke();
+// 집 그리기
+// 벽
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+
+// 문
+// ctx.linewidth = 2; // 선 두께 조정
+ctx.fillRect(300, 300, 50, 100);
+
+// 천장
+ctx.fillRect(200, 200, 200, 20);
+
+// 지붕(삼각형)
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
