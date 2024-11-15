37eedf56f90d08693839e2112902655216eb8ddc
yeonjae <pyj3740@gmail.com>
2024-11-15 10:46:48 +0900

    2.2: Line Width

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,10 +1,11 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용
 
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2; // 선 두께
+ctx.lineWidth = lineWidth.value; // 선 두께
 
 let isPainting = false; // 유저의 painting 상태
 
@@ -14,6 +15,7 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -25,8 +27,15 @@ function cancelPainting() {
   isPainting = false; // 그림 안그리는 중
 }
 
+function onLineWidthChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 // document.addEventListener("mouseleave", cancelPainting)
+
+lineWidth.addEventListener("change", onLineWidthChange);
